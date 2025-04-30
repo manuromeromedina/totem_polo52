8d09eddad094bcb1c10b1e6e0accef6bca5947f1
Josefina Rivero <josefinarivero03@gmail.com>
2025-04-30 14:14:48 -0300

    front

--- frontend/src/routes/AppRouter.jsx
+++ frontend/src/routes/AppRouter.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LoginPage from '../pages/LoginPage'
-import RegisterPage from '../pages/RegisterPage'
+import RegisterPage from '../Pages/RegisterPage'
 
 const AppRouter = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
+      
     </Routes>
   </BrowserRouter>
 )
