84b8a34dca2b54873061086d1df679e1d8dde4f5
Josefina Rivero <josefinarivero03@gmail.com>
2025-04-30 15:11:28 -0300

    se agrego front

--- frontend/src/routes/AppRouter.jsx
+++ frontend/src/routes/AppRouter.jsx
@@ -1,12 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LoginPage from '../pages/LoginPage'
 import RegisterPage from '../Pages/RegisterPage'
+import ForgotpasswordPage from '../pages/ForgotpasswordPage'
+
 
 const AppRouter = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
+      <Route path="/forgot-password" element={<ForgotpasswordPage />} />
+      <Route path="/recuperar" element={<ForgotpasswordPage />} />
+
       
     </Routes>
   </BrowserRouter>
