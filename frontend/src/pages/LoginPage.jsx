a6e5e7dd114bec1bd65d56a5ec55f8d17fd6878b
Josefina Rivero <josefinarivero03@gmail.com>
2025-05-05 11:03:41 -0300

    front

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -4,6 +4,9 @@ import { login } from '../services/authService'
 import Input from '../components/Input'
 import Button from '../components/Button'
 import { Link } from 'react-router-dom';
+import '../styles/main.css' 
+import '../styles/AlertModal.css' 
+
 
 const LoginPage = () => {
   const [email, setEmail] = useState('')
@@ -27,11 +30,13 @@ const LoginPage = () => {
       <Input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Correo" />
       <Input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Contraseña" />
       <Button type="submit">Iniciar sesión</Button>
+      <p className='texto-chico'>
       <p>
         ¿No tienes cuenta? <a href="/register">Registrate aquí</a>  
       </p>
-      <p>
+      <p >
         ¿Olvidaste tu contraseña? <Link to="/recuperar">Recuperala aquí</Link>
+      </p>
       </p>
     </form>
   )
