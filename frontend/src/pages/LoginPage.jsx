ae2b60bee992b98d7f9abac9681deebf90781689
Josefina Rivero <josefinarivero03@gmail.com>
2025-05-05 15:30:28 -0300

    fornt

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -6,11 +6,15 @@ import Button from '../components/Button'
 import { Link } from 'react-router-dom';
 import '../styles/main.css' 
 import '../styles/AlertModal.css' 
+import '../styles/modal.css' 
+import ModalRecuperarContraseña from '../components/ForgotpasswordPage'
+
 
 
 const LoginPage = () => {
   const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const [password, setPassword] = useState('')  
+  const [showModal, setShowModal] = useState(false)
   const navigate = useNavigate()
 
   const handleSubmit = async (e) => {
@@ -22,23 +26,31 @@ const LoginPage = () => {
     } catch (error) {
       alert('Error al iniciar sesión')
     }
+
   }
 
+
   return (
+    <>
     <form onSubmit={handleSubmit} className="form-container">
       <h2>Iniciar sesión</h2>
       <Input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Correo" />
       <Input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Contraseña" />
       <Button type="submit">Iniciar sesión</Button>
       <p className='texto-chico'>
-      <p>
         ¿No tienes cuenta? <a href="/register">Registrate aquí</a>  
       </p>
-      <p >
-        ¿Olvidaste tu contraseña? <Link to="/recuperar">Recuperala aquí</Link>
-      </p>
+      <p className='texto-chico'>
+         ¿Olvidaste tu contraseña?{' '}
+         <span onClick={() => setShowModal(true)}>
+           Recuperala aquí
+        </span>
       </p>
     </form>
+    <ModalRecuperarContraseña visible={showModal} onClose={() => setShowModal(false)} />
+    </>
+
+    
   )
 }
 
