b644e2c4a9008373f079e0f22bfdb2f58a2b1e68
mquelas <maxiquelas80@gmail.com>
2025-06-12 19:22:26 -0300

    front

--- frontend/src/app/app.modules.ts
+++ frontend/src/app/app.modules.ts
@@ -1,17 +1,41 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms'; 
-import { AppComponent } from './app.component';
+ // src/app/app.module.ts
+ import { NgModule } from '@angular/core';
+ import { BrowserModule } from '@angular/platform-browser';
+ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+ import { RouterModule } from '@angular/router';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
+import { AppComponent }    from './app.component';  // es standalone
+
+ import { AuthInterceptor } from './auth/auth.interceptor';
+
+ @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+   AppComponent,        // ← aquí lo importas
+    HttpClientModule,
+    RouterModule.forRoot([
+      {
+        path: 'login',
+        loadComponent: () =>
+          import('./auth/login/login.component').then(m => m.LoginComponent)
+      },
+      {
+        path: 'register',
+        loadComponent: () =>
+          import('./auth/register/register.component').then(m => m.RegisterComponent)
+      },
+      {
+        path: 'dashboard',
+        loadChildren: () =>
+          import('./dashboard/dashboard.module').then(m => m.DashboardModule)
+      },
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+      { path: '**', redirectTo: 'login' }
+    ])
+  ],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
   ],
   bootstrap: [AppComponent]
-})
-export class AppModule {}
-
+ })
+ export class AppModule {}
